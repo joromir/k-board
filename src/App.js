c07f6fac41fdfc38b99634d6e1e692dd7697f22d
Georgi N. Kostov <gnkostov@yahoo.com>
2017-02-14 09:31:31 +0200

    Define BoardColumn component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BoardColumn from './BoardColumn';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>K-Board</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <BoardColumn />
       </div>
     );
   }
---
+++ src/BoardColumn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BoardColumn extends Component {
+  render() {
+    return(
+      <div className="BoardColumn">
+        <h1>BoardName</h1>
+      </div>
+    );
+  }
+}
+
+export default BoardColumn;
